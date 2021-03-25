05df51338310a7a2a053bac85ddb65d8987ed073
Jerry <w6725181@gmail.com>
2021-03-26 02:07:39 +1100

    lab3 finished

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,9 @@
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
+        //return a.equals(b); or (int a, int b) works
+        // == two object points to same instance
+        // data type is Integer not int. Autoboxing caches -128 to 127
         return a == b;
     }
 }
